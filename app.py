8249e60801c66c7f8d6c90f002d04448442b9e6e
ruchitha2912 <96610982+ruchitha2912@users.noreply.github.com>
2023-10-24 20:56:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import mysql.connector
 
 app = Flask(__name__)
@@ -7,22 +7,26 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="password",
+    password="YourPassword",
     database="registered_vehicles"
 )
+
 if db.is_connected():
     print("Connected to the database")
 else:
     print("Failed to connect to the database")
+
+
+
 cursor = db.cursor()
 
 @app.route('/')
 def index():
-    # Display existing data from the database
-    cursor.execute("SELECT * FROM owners")
-    table_data = cursor.fetchall()
-    column_names = [description[0] for description in cursor.description]
-    return render_template('index.html', column_names=column_names, table_data=table_data)
+    # Fetch data from the database
+    cursor.execute("SELECT * FROM Vehicle")
+    columns = [desc[0] for desc in cursor.description]
+    data = cursor.fetchall()
+    return render_template('index.html', columns=columns, data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
