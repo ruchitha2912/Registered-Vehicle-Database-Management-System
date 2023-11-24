e373df5dc5da2e0bd197b83747f82247d40b678e
Ashwini Hegde <112482378+ashwinihegde02@users.noreply.github.com>
2023-11-24 20:10:05 +0530

    Final files

--- admin.py
+++ admin.py
@@ -13,29 +13,33 @@ import random
 import warnings
 warnings.filterwarnings("ignore")
 
+
 def connect_db():
     db = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Yogadeepa@210",
+        password="ashwini",
         database="registered_vehicles"
     )
 
     return db
 
 # get all tables of database
-def get_tables(cursor,operation):
+
+
+def get_tables(cursor, operation):
     cursor.execute("SHOW TABLES;")
     tables = [table[0] for table in cursor.fetchall()]
 
-    if operation == "Update data" :
-        relevant_tables = ['accident','inspection','insurance','license','maintenance','owners','registration','vehicle','violation']
+    if operation == "Update data":
+        relevant_tables = ['accident', 'inspection', 'insurance', 'license',
+                           'maintenance', 'owners', 'registration', 'vehicle', 'violation']
         return [table for table in tables if table in relevant_tables]
 
     if operation == "Insert data":
-        relevant_tables = ['accident','violation']
+        relevant_tables = ['accident', 'violation']
         return [table for table in tables if table in relevant_tables]
-    
+
     return tables
 
 # function to get column names
@@ -59,7 +63,7 @@ def fetch_all_data(conn, table):
     result = None
     for result_cursor in cursor.stored_results():
         result = result_cursor.fetchall()
-        
+
     cursor.execute(
         f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table}'")
     column_names = [column[0] for column in cursor.fetchall()]
@@ -70,17 +74,23 @@ def fetch_all_data(conn, table):
     return result
 
 # Function to generate random alphanumeric ID
+
+
 def generate_random_id():
     return str(uuid.uuid4().hex.upper()[0:4])
+
 
 def gen_random_license():
     return str(random.randint(10000, 99999))
 
+
 def gen_random_reg():
     return str(uuid.uuid4().hex.upper()[0:6])
 
+
 def gen_random_insp():
     return str(uuid.uuid4().hex.upper()[0:4])
+
 
 def gen_random_accident_id():
     return str(uuid.uuid4().hex.upper()[0:3])
@@ -95,6 +105,7 @@ def fetch_owner_id_by_license(cursor, license_num):
     else:
         return None
 
+
 def fetch_vehicles_by_owner(cursor, owner_id):
     query = "SELECT Vehicle_id, NumberPlate FROM vehicle WHERE owner_id = %s"
     cursor.execute(query, (owner_id,))
@@ -105,6 +116,8 @@ def fetch_vehicles_by_owner(cursor, owner_id):
         return None
 
 # Function to add a new row to the database
+
+
 def insert_data(db, cursor, table, column):
     st.subheader('Insert New Record')
     if table == 'accident':
@@ -120,10 +133,12 @@ def insert_data(db, cursor, table, column):
 
             if vehicles:
                 # Create a list of vehicle options for the dropdown
-                vehicle_options = [f"{vehicle[0]} - {vehicle[1]}" for vehicle in vehicles]
+                vehicle_options = [
+                    f"{vehicle[0]} - {vehicle[1]}" for vehicle in vehicles]
 
                 # Allow admin to choose from the list of vehicles
-                selected_vehicle = st.selectbox('Select Vehicle', vehicle_options)
+                selected_vehicle = st.selectbox(
+                    'Select Vehicle', vehicle_options)
 
                 # Extract vehicle_id from the selected option
                 vehicle_id = selected_vehicle.split(' - ')[0]
@@ -132,7 +147,8 @@ def insert_data(db, cursor, table, column):
                 acc_date = st.date_input('Accident Date')
                 acc_desc = st.text_input('Description about the Accident')
 
-                add_cols = [acc_id, license_num, vehicle_id, location, acc_date, acc_desc]
+                add_cols = [acc_id, license_num, vehicle_id,
+                            location, acc_date, acc_desc]
                 if st.button('Add data'):
                     if add_cols:
                         insert_query = "INSERT INTO accident VALUES (%s, %s, %s, %s, %s, %s)"
@@ -141,10 +157,12 @@ def insert_data(db, cursor, table, column):
                     else:
                         st.warning('Please fill in all the details.')
             else:
-                st.warning(f'No vehicles found for the owner with ID: {owner_id}')
+                st.warning(
+                    f'No vehicles found for the owner with ID: {owner_id}')
         else:
             if license_num:
-                st.warning(f'No owner found for the given license number: {license_num}')
+                st.warning(
+                    f'No owner found for the given license number: {license_num}')
 
     elif table == 'violation':
         violation_id = gen_random_insp()
@@ -154,9 +172,9 @@ def insert_data(db, cursor, table, column):
         violation_type = st.text_input('Vioilation Type')
         st.success(f"Generated Violation ID: {violation_id}")
 
+        add_cols = [violation_id, Vehicle_id,
+                    fine_amt, violation_date, violation_type]
 
-        add_cols = [violation_id,Vehicle_id, fine_amt,violation_date, violation_type]
-        
         if st.button('Add data'):
             if add_cols:
                 insert_query = "INSERT INTO violation VALUES (%s, %s, %s, %s, %s)"
@@ -164,8 +182,9 @@ def insert_data(db, cursor, table, column):
                 st.success('Details added successfully!')
             else:
                 st.warning('Please fill in all the details.')
-    
+
     db.commit()
+
 
 def fetch_vehicle_ids_by_owner(cursor, owner_id):
     query = "SELECT Vehicle_id FROM Vehicle WHERE Owner_id = %s"
@@ -176,19 +195,22 @@ def fetch_vehicle_ids_by_owner(cursor, owner_id):
     else:
         return []
 
+
 def delete_data(db, cursor):
     owner_id = st.text_input('Enter Owner ID')
     vehicle_ids = fetch_vehicle_ids_by_owner(cursor, owner_id)
 
     if vehicle_ids:
-        selected_vehicle_id = st.selectbox('Select Vehicle ID to Delete', vehicle_ids)
+        selected_vehicle_id = st.selectbox(
+            'Select Vehicle ID to Delete', vehicle_ids)
 
         if st.button('Delete Vehicle Data'):
             try:
                 # Delete data from child tables first
                 cursor.callproc('DeleteVehicleDetails', [selected_vehicle_id])
                 # print(selected_vehicle_id)
-                st.success(f'Data for Vehicle ID {selected_vehicle_id} deleted successfully!')
+                st.success(
+                    f'Data for Vehicle ID {selected_vehicle_id} deleted successfully!')
                 db.commit()
 
             except Exception as e:
@@ -198,14 +220,15 @@ def delete_data(db, cursor):
         if owner_id:
             st.warning(f'No vehicles found for Owner ID: {owner_id}')
 
+
 def fetch_data(cursor, query):
     cursor.execute(query)
     result = cursor.fetchall()
     return result
-            
+
 
 def update_data(db, cursor, table, column):
-    #code here!
+    # code here!
     st.subheader(f'Update {table} Data')
     # Display existing data
     st.write("Existing Data:")
@@ -214,9 +237,9 @@ def update_data(db, cursor, table, column):
     existing_data = pd.DataFrame(existing_data)
     st.table(existing_data)
 
-    details = {"owners":"owner_id","accident":"accident_id","insurance":"policy_number",
-               "inspection":"inspection_id","license":"license_number","maintenance":"service_id",
-               "registration":"register_id","vehicle":"vehicle_id","violation":"violation_id"}
+    details = {"owners": "owner_id", "accident": "accident_id", "insurance": "policy_number",
+               "inspection": "inspection_id", "license": "license_number", "maintenance": "service_id",
+               "registration": "register_id", "vehicle": "vehicle_id", "violation": "violation_id"}
     # Choose the record to update
     record_id = st.text_input(f'Enter {details[table]} to Update')
     if not record_id:
@@ -228,18 +251,21 @@ def update_data(db, cursor, table, column):
     update_options = {}
     for col in column:
         update_options[col] = st.text_input(f'Update {col}')
-    update_options = {k: v for k, v in update_options.items() if v}  # Remove empty values
+    # Remove empty values
+    update_options = {k: v for k, v in update_options.items() if v}
 
     # Update the record
     if st.button('Update Data'):
         try:
-            set_clause = ", ".join([f"{col} = %s" for col in update_options.keys()])    
+            set_clause = ", ".join(
+                [f"{col} = %s" for col in update_options.keys()])
             update_query = f"UPDATE {table} SET {set_clause} WHERE {details[table]}= %s"
             print(update_query)
             update_values = list(update_options.values()) + [record_id]
             cursor.execute(update_query, update_values)
             db.commit()
-            st.success(f'Data for {table} ID {record_id} updated successfully!')
+            st.success(
+                f'Data for {table} ID {record_id} updated successfully!')
         except Exception as e:
             st.warning(f'Error: {e}')
 
@@ -248,7 +274,7 @@ def validate_email(email):
     return '@' in email
 
 
-def insert_new_details(db,cursor):
+def insert_new_details(db, cursor):
     st.subheader('Insert New Details')
     # Insert details for owners
     st.subheader('Owner Details:')
@@ -257,14 +283,14 @@ def insert_new_details(db,cursor):
     owner_phone = st.text_input('Owner Phone number')
     if owner_phone:
         if len(owner_phone) != 10 or not owner_phone.isdigit():
-            st.warning('Invalid phone number format. Please enter a 10-digit number.')
+            st.warning(
+                'Invalid phone number format. Please enter a 10-digit number.')
             return
     owner_mail = st.text_input('Owner Mail')
     if owner_mail:
         if not validate_email(owner_mail):
             st.warning('Invalid email format. Please enter a valid email.')
             return
-        
 
     license_num = 'LIC' + gen_random_license()
     gender = st.selectbox('Gender', ['M', 'F'])
@@ -274,11 +300,12 @@ def insert_new_details(db,cursor):
     if age <= 18:
         st.warning('Owner must be at least 18 years old.')
         return
-    
+
     st.success(f"Generated Owner ID: {owner_id}")
     st.success(f"Generated License Number: {license_num}")
 
-    owner_data = [owner_id, owner_name, owner_phone, license_num, owner_mail, gender, address, dob]
+    owner_data = [owner_id, owner_name, owner_phone,
+                  license_num, owner_mail, gender, address, dob]
 
     # Insert details for vehicle
     st.subheader('Vehicle Details:')
@@ -292,7 +319,8 @@ def insert_new_details(db,cursor):
     VehicleType = st.text_input('Vehicle Type (Motorcycle / Car)')
     Vehicle_description = st.text_area('Vehicle Description')
 
-    vehicle_data = [Vehicle_id,RegNumber,model, owner_id, RegDate,NumberPlate, VehicleType, Vehicle_description]
+    vehicle_data = [Vehicle_id, RegNumber, model, owner_id,
+                    RegDate, NumberPlate, VehicleType, Vehicle_description]
 
     # Insert details for license
     st.subheader('License Details:')
@@ -314,7 +342,8 @@ def insert_new_details(db,cursor):
     st.subheader('Inspection Details:')
     insp_id = 'INSP' + gen_random_insp()
     insp_name = st.text_input('Inspector Name')
-    result = st.text_input('Result of the Inspection (1 for Positive result and 0 for Negative result)')
+    result = st.text_input(
+        'Result of the Inspection (1 for Positive result and 0 for Negative result)')
     insp_date = st.date_input('Inspection Date')
     st.success(f"Generated Inspection ID: {insp_id}")
 
@@ -366,14 +395,13 @@ def insert_new_details(db,cursor):
             st.warning(f'Error: {e}')
 
 
-
 # handle different operations on table
-def handle_table(db, cursor,option):
+def handle_table(db, cursor, option):
 
-    tables = get_tables(cursor,option)
+    tables = get_tables(cursor, option)
     if option == 'Insert data':
         tables.append("new details")
-    
+
     if option == 'View data':
         table = st.sidebar.selectbox('Select Table', tables)
         # Display all data in table
@@ -401,7 +429,14 @@ def handle_table(db, cursor,option):
         columns = get_column_names(db, table)
         # st.subheader('Update data')
         update_data(db, cursor, table, columns)
+    elif option == 'Execute a query':
+        query = st.text_input('Write your Query: ')
+        result = None
+        if query:
+            result = pd.read_sql_query(query, db)
+            st.dataframe(result)
     return
+
 
 def fetch_accidents_by_month(cursor):
     query = """
@@ -423,48 +458,51 @@ def fetch_violations_by_month(cursor):
     cursor.execute(query)
     results = cursor.fetchall()
     return pd.DataFrame(results, columns=['Month', 'Count'])
- 
+
+
 def analyze(cursor):
-        st.empty()
-        query = "SELECT Gender, COUNT(*) as Count FROM owners GROUP BY Gender"
-        cursor.execute(query)
-        results = cursor.fetchall()
+    st.empty()
+    query = "SELECT Gender, COUNT(*) as Count FROM owners GROUP BY Gender"
+    cursor.execute(query)
+    results = cursor.fetchall()
 
-        # Create a DataFrame for visualization
-        df = pd.DataFrame(results, columns=['Gender', 'Count'])
-        # st.table(df)
-        st.set_option('deprecation.showPyplotGlobalUse', False)
+    # Create a DataFrame for visualization
+    df = pd.DataFrame(results, columns=['Gender', 'Count'])
+    # st.table(df)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
 
+    # Plot the bar graph using Seaborn
+    plt.figure(figsize=(4, 2))
+    sns.barplot(x='Gender', y='Count', data=df, linewidth=2,
+                edgecolor="0.2", color='skyblue')
+    plt.title('Number of Male and Female Drivers')
+    plt.xlabel('Gender')
+    plt.ylabel('Count')
+    # Assuming 0 for Male and 1 for Female
+    plt.xticks([0, 1], ['Male', 'Female'])
+    st.pyplot()
 
-        # Plot the bar graph using Seaborn
-        plt.figure(figsize=(4, 2))
-        sns.barplot(x='Gender', y='Count', data=df, linewidth=2, edgecolor="0.2", color='skyblue')
-        plt.title('Number of Male and Female Drivers')
-        plt.xlabel('Gender')
-        plt.ylabel('Count')
-        plt.xticks([0, 1], ['Male', 'Female'])  # Assuming 0 for Male and 1 for Female
-        st.pyplot()
+    accidents_by_month = fetch_accidents_by_month(cursor)
+    # Plot the bar chart
+    plt.figure(figsize=(4, 2))
+    sns.barplot(x='Month', y='Count', data=accidents_by_month,
+                linewidth=2, edgecolor="0.2", color='skyblue')
+    plt.title('Number of Accidents by Month')
+    plt.xlabel('Month')
+    plt.ylabel('Count')
+    st.pyplot()
+    violations_by_month = fetch_violations_by_month(cursor)
+    # Plot the bar chart
+    plt.figure(figsize=(4, 2))
+    sns.barplot(x='Month', y='Count', data=violations_by_month,
+                linewidth=2, edgecolor="0.2", color='skyblue')
+    plt.title('Number of Violations by Month')
+    plt.xlabel('Month')
+    plt.ylabel('Count')
 
-        accidents_by_month = fetch_accidents_by_month(cursor)
-        # Plot the bar chart
-        plt.figure(figsize=(4, 2))
-        sns.barplot(x='Month', y='Count', data=accidents_by_month, linewidth=2, edgecolor="0.2", color='skyblue')
-        plt.title('Number of Accidents by Month')
-        plt.xlabel('Month')
-        plt.ylabel('Count')
-        st.pyplot()
-        violations_by_month = fetch_violations_by_month(cursor)
-        # Plot the bar chart
-        plt.figure(figsize=(4, 2))
-        sns.barplot(x='Month', y='Count', data=violations_by_month, linewidth=2, edgecolor="0.2", color='skyblue')
-        plt.title('Number of Violations by Month')
-        plt.xlabel('Month')
-        plt.ylabel('Count')
+    # Display the plot in the Streamlit app
+    st.pyplot()
 
-        # Display the plot in the Streamlit app
-        st.pyplot()
-
-    
 
 # Streamlit app
 def admin_dashboard():
@@ -483,14 +521,15 @@ def admin_dashboard():
         # Sidebar menu
         st.sidebar.title('Menu')
         option = st.sidebar.selectbox(
-            'Select Operation', ['View data', 'Insert data', 'Update data', 'Delete data','Analyze data'])
+            'Select Operation', ['View data', 'Insert data', 'Update data', 'Delete data', 'Analyze data', 'Execute a query'])
 
         # table = st.sidebar.selectbox('Select Table', tables)
 
-        handle_table(db, cursor,option)
+        handle_table(db, cursor, option)
         db.close()
     else:
         st.warning("Please Log In to access the pages.")
+
 
 if __name__ == '__main__':
     st.set_page_config(
